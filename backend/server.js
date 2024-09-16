f8e0356d2df3aa74f15a8295ee02d1584c52ab9c
Madhawa-Tharindu <madhawath@gmail.com>
2024-09-16 20:48:13 +0530

    changes on server.js for the deployment

--- backend/server.js
+++ backend/server.js
@@ -50,8 +50,7 @@ if (process.env.NODE_ENV == 'production') {
 }
 
 // Server listening on port
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-	connectToMongoDB();
-	console.log(`Server Running on port ${PORT}`);
-});
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.log('Error connecting to MongoDB', err));
+
